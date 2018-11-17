77bca53dba29b60742e0f4821957b965eccbdc4a
THE REAL K94 <kurdt94@gmail.com>
2018-11-17 05:48:27 +0100

    #

--- MushRoom/Form1.Designer.cs
+++ MushRoom/Form1.Designer.cs
@@ -54,9 +54,10 @@
             // 
             // button1
             // 
+            this.button1.BackColor = System.Drawing.Color.LightGreen;
             resources.ApplyResources(this.button1, "button1");
             this.button1.Name = "button1";
-            this.button1.UseVisualStyleBackColor = true;
+            this.button1.UseVisualStyleBackColor = false;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // statusStrip1
